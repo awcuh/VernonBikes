198a4e0afdd46964ec54495694e920ea60b48ffe
tku <tku@tkus-iMac.local>
2020-01-30 04:26:21 -0600

    counter

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$handl = fopen("counter.txt", "r");
+if(!$handle){
+echo "could not open the file" ;
+}
+else {
+    $counter = (int ) fread($handle,20);
+    fclose ($handle);
+    $counter++;
+    echo" <strong> This Website has been visited  " . $counter . " times</strong> " ;
+$handle = fopen("counter.txt", "w");
+fwrite($handle,$counter) ;
+fclose ($handle) ;
+    }
+?>
